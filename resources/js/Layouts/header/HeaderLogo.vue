4e22126787fe5dccd577551ac04760871cc433f9
ObedientPutro <obedientjunior@gmail.com>
2026-02-02 08:40:17 +0800

    change: app header logo

--- resources/js/Layouts/header/HeaderLogo.vue
+++ resources/js/Layouts/header/HeaderLogo.vue
@@ -4,10 +4,10 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <Link to="/" class="lg:hidden">
-        <img class="dark:hidden" src="/images/logo/logo.svg" alt="Logo" />
+        <img class="dark:hidden h-9 w-auto mx-auto" src="/images/logo/logo-light.png" alt="Logo" />
         <img
-            class="hidden dark:block"
-            src="/images/logo/logo-dark.svg"
+            class="hidden dark:block h-9 w-auto mx-auto"
+            src="/images/logo/logo-dark.png"
             alt="Logo"
         />
     </Link>
